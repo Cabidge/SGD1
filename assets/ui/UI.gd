f454f8b9cc041cc6dc4e861756558c621622b8e9
wdong20 <wdong20@stuy.edu>
2020-11-13 11:50:59 -0500

    Created simple mana label.

--- assets/ui/UI.gd
+++ assets/ui/UI.gd
@@ -1,2 +1,9 @@
 extends CanvasLayer
 
+onready var mana = $Control/Mana
+
+func _ready():
+	Player.connect("updated_mana",self,"_on_Player_updated_mana")
+
+func _on_Player_updated_mana(new):
+	mana.text = "Mana: " + str(new)
