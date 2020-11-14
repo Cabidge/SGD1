b7b3c94670268b73490372132faec9acad419d5b
wdong20 <wdong20@stuy.edu>
2020-11-13 20:09:20 -0500

    Created vitals bars.

--- assets/ui/UI.gd
+++ assets/ui/UI.gd
@@ -1,10 +1,22 @@
 extends Control
 
-onready var mana = $Mana
+onready var vitals = $Vitals
 
 func _ready():
 	var err = Player.connect("updated_mana",self,"_on_Player_updated_mana")
 	assert(err == OK)
+	
+	err = Player.connect("updated_health",self,"_on_Player_updated_health")
+	assert(err == OK)
+	
+	err = Player.connect("updated_stealth",self,"_on_Player_updated_stealth")
+	assert(err == OK)
 
-func _on_Player_updated_mana(new):
-	mana.text = "Mana: " + str(new)
+func _on_Player_updated_stealth(stealth):
+	vitals.update_stealth(stealth)
+
+func _on_Player_updated_mana(mana):
+	vitals.update_mana(mana)
+
+func _on_Player_updated_health(health):
+	vitals.update_health(health)
--- assets/ui/vitals/Vitals.gd
+++ assets/ui/vitals/Vitals.gd
@@ -1,12 +1,32 @@
 extends Control
 
 onready var portrait_anim = $Portrait/AnimationPlayer
+onready var mana_bars = [$ManaWhite,$ManaProgress]
+onready var health_bars = [$HealthWhite,$HealthProgress]
 
 func _ready():
-	Player.connect("updated_stealth",self,"_on_Player_updated_stealth")
+	for mana in mana_bars:
+		mana.max_value = Player.MAX_MANA
+		mana.value = Player.mana
+		mana.step = 0.01
+	
+	for health in health_bars:
+		health.max_value = Player.MAX_HEALTH
+		health.value = Player.health
+		health.step = 0.01
 
-func _on_Player_updated_stealth(stealth):
+func _process(delta):
+	mana_bars[0].value = max(mana_bars[1].value,mana_bars[0].value - delta * 2)
+	health_bars[0].value = max(health_bars[1].value,health_bars[0].value - delta * 2)
+
+func update_stealth(stealth):
 	if stealth:
 		portrait_anim.play("Transition")
 	else:
 		portrait_anim.play_backwards("Transition")
+
+func update_mana(mana):
+	mana_bars[1].value = mana
+
+func update_health(health):
+	health_bars[1].value = health
