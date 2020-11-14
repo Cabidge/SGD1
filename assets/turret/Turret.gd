fc5d91d1833c97cec0067cffa1981132979fe8d4
wdong20 <wdong20@stuy.edu>
2020-11-14 12:14:33 -0500

    Added turret scanning.

--- assets/turret/Turret.gd
+++ assets/turret/Turret.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const SCAN_ANGLE = 0.2
-const SCAN_TIME = 0.8
+const SCAN_ANGLE = deg2rad(45)
+const SCAN_TIME = 1.0
 
 export var central_angle = 0.0
 
@@ -13,17 +13,20 @@ onready var laser = $Laser
 onready var tween = $Tween
 
 func _ready():
+	set_angle(central_angle)
 	scan_auto()
 
 func scan_auto():
+	print(angle)
 	if angle < central_angle:
 		tween_angle(central_angle + SCAN_ANGLE)
 	else:
 		tween_angle(central_angle - SCAN_ANGLE)
 
-func tween_angle(angle_diff : float):
-	tween.interpolate_property(self,"angle",angle,angle + angle_diff,
-			SCAN_TIME, Tween.TRANS_SINE,Tween.EASE_IN_OUT)
+func tween_angle(new_angle : float):
+	var time = abs((angle - new_angle) / SCAN_ANGLE) * SCAN_TIME
+	tween.interpolate_property(self,"angle",angle,new_angle,
+			time)
 	tween.start()
 
 func set_angle(new):
