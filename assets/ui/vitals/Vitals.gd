0d531c0e96c96f1da65044ddc0d502ce79f91f76
wdong20 <wdong20@stuy.edu>
2020-11-16 21:36:33 -0500

    Changed mana increase rate

--- assets/ui/vitals/Vitals.gd
+++ assets/ui/vitals/Vitals.gd
@@ -20,7 +20,7 @@ func _ready():
 
 func _process(delta):
 	mana_bars[0].value = max(mana_bars[1].value,mana_bars[0].value - delta * 2)
-	mana_bars[1].value = min(mana_count,mana_bars[1].value + delta * 3)
+	mana_bars[1].value = min(mana_count,mana_bars[1].value + delta * 2)
 	
 	health_bars[0].value = max(health_bars[1].value,health_bars[0].value - delta * 5)
 	health_bars[1].value = min(health_count,health_bars[1].value + delta * 2)
