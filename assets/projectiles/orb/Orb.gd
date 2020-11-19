80eb438812553d1bda40579f450504a20b4f5f19
wdong20 <wdong20@stuy.edu>
2020-11-19 16:22:06 -0500

    Deflection

--- assets/projectiles/orb/Orb.gd
+++ assets/projectiles/orb/Orb.gd
@@ -4,6 +4,8 @@ const KNOCKBACK = 128
 
 var hit_info = HitInfo.new(2)
 
+onready var sprite = $AnimatedSprite
+
 func _physics_process(delta):
 	handle_collision(delta)
 
@@ -12,5 +14,14 @@ func _on_Orb_collided(collision):
 		collision.collider.hit(hit_info)
 
 func _on_Orb_fired(vec):
-	position += vec * 12
+	position += vec * 8
 	hit_info.knockback = vec * KNOCKBACK
+
+
+func deflect(angle):
+	fire_at_angle(angle)
+	
+	# tweak collisions
+	set_collision_layer_bit(7, false) # turn off layer
+	set_collision_mask_bit(5, false) # turn off player collision
+	set_collision_mask_bit(6, true) # turn on enemy collision
