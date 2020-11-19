994e7cf7e4ec5cc353c304070283c7b2503e09bf
wdong20 <wdong20@stuy.edu>
2020-11-18 19:31:02 -0500

    Stored parent as onready variable

--- assets/guard/GuardStabRange.gd
+++ assets/guard/GuardStabRange.gd
@@ -2,8 +2,13 @@ extends "res://assets/backstab/StabRange.gd"
 
 const BACKSTAB_ANGLE = deg2rad(65)
 
+onready var parent = get_parent()
+
 func can_stab(player : Character2D) -> bool:
+	if parent.state_machine.state == parent.state_machine.states.attack:
+		return false
+	
 	var player_vec = (player.position - get_parent().position)
-	var back_normal = Vector2.LEFT.rotated(get_parent().angle)
+	var back_normal = Vector2.LEFT.rotated(parent.angle)
 	var angle = player_vec.angle_to(back_normal)
 	return abs(angle) <= BACKSTAB_ANGLE
