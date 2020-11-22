cdcc45d31bcb0b5aff508d5dedf1bdf1cff2bcab
wdong20 <wdong20@stuy.edu>
2020-11-22 15:22:00 -0500

    Setup alerting system

--- assets/audio/AudioAlert2D.gd
+++ assets/audio/AudioAlert2D.gd
@@ -1,6 +1,24 @@
+tool
 extends AudioStreamPlayer2D
 
+export var alert_level = 0
+export var radius = 0.0 setget set_radius, get_radius
 export var looping = false
+
+onready var area = $Area2D
+
+func set_radius(new):
+	$Area2D/CollisionShape2D.shape.radius = new
+
+func get_radius():
+	return $Area2D/CollisionShape2D.shape.radius
+
+
+func play(from_positon : float = 0.0):
+	.play(from_positon)
+	if radius > 0:
+		for alert_receiver in area.get_overlapping_areas():
+			alert_receiver.alert(alert_level, global_position)
 
 func loop():
 	play()
