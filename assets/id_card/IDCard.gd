f49cf79f5572f9ec5e48f90e5982820f157f6d8e
wdong20 <wdong20@stuy.edu>
2020-11-25 10:29:57 -0500

    Drops now when not a child to guard

--- assets/id_card/IDCard.gd
+++ assets/id_card/IDCard.gd
@@ -8,29 +8,39 @@ func _ready():
 	var parent = get_parent()
 	if parent.is_in_group("Guard"):
 		parent.connect("died",self,"_on_Guard_died")
+	else:
+		drop()
 
 
 func _on_Guard_died():
 	if interact_area.is_in_range():
 		pick_up()
 	else:
-		sprite.position.y = -4
-		interact_area.disabled = false
-		$Shadow.visible = true
+		drop()
 	
-	position = get_parent().position
-	
-	var new_parent = get_parent().get_parent()
-	get_parent().remove_child(self)
-	new_parent.add_child(self)
+	reparent()
 
 func pick_up():
 	sprite.visible = false
 	$Timer.start()
 	Player.has_id = true
 
+func drop():
+	sprite.position.y = -4
+	interact_area.disabled = false
+	$Shadow.visible = true
+
+func reparent():
+	position = get_parent().position
+	
+	var new_parent = get_parent().get_parent()
+	get_parent().remove_child(self)
+	new_parent.add_child(self)
+
+
 func _on_InteractArea2D_interacted():
 	pick_up()
+	$Shadow.visible = false
 	interact_area.disabled = true
 
 func _on_Timer_timeout():
