5bf5d18d628f8b233258558054bd9a40a7b3e4b2
wdong20 <wdong20@stuy.edu>
2020-11-24 22:43:47 -0500

    Check if player has ID

--- assets/elevator/exit/ExitElevator.gd
+++ assets/elevator/exit/ExitElevator.gd
@@ -6,7 +6,7 @@ onready var elevator = $Elevator
 onready var player_tween_timer = $PlayerTweenTimer
 
 func _on_PlayerDetector_body_entered(body):
-	if !body.stealth:
+	if !body.stealth and Player.has_id:
 		elevator.tween_player_sprite(body.sprite)
 		elevator.enable_camera = true
 		
