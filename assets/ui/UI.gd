0ebe3bb2d7ebb435ce6c4e184b9980618a802496
wdong20 <wdong20@stuy.edu>
2020-11-24 22:36:18 -0500

    ID animation

--- assets/ui/UI.gd
+++ assets/ui/UI.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var vitals = $Vitals
+onready var id_display_anim = $ID/AnimationPlayer
 
 func _ready():
 	var err = Player.connect("updated_mana",self,"_on_Player_updated_mana")
@@ -10,6 +11,9 @@ func _ready():
 	assert(err == OK)
 	
 	err = Player.connect("updated_stealth",self,"_on_Player_updated_stealth")
+	assert(err == OK)
+	
+	err = Player.connect("updated_id",self,"_on_Player_updated_id")
 	assert(err == OK)
 
 func reset_all():
@@ -25,3 +29,10 @@ func _on_Player_updated_mana(mana):
 
 func _on_Player_updated_health(health):
 	vitals.update_health(health)
+
+func _on_Player_updated_id(has_id):
+	if has_id:
+		id_display_anim.play("Show")
+	else:
+		id_display_anim.play_backwards("Show")
+		id_display_anim.seek(0)
