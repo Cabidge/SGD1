17dc6f94f646f596113055ac6a6034855f9c9a97
wdong20 <wdong20@stuy.edu>
2020-11-24 22:26:53 -0500

    has_id variable

--- Player.gd
+++ Player.gd
@@ -3,12 +3,15 @@ extends Node
 signal updated_health(new)
 signal updated_mana(new)
 signal updated_stealth(new)
+signal updated_id(new)
 
 const MAX_HEALTH = 10
 const MAX_MANA = 10
 
 var health := MAX_HEALTH setget set_health
 var mana := MAX_MANA setget set_mana
+
+var has_id := false setget set_id
 
 func set_health(new : int):
 	new = int(clamp(new,0,MAX_HEALTH))
@@ -24,3 +27,8 @@ func set_mana(new : int):
 
 func update_stealth(stealth):
 	emit_signal("updated_stealth",stealth)
+
+
+func set_id(new):
+	has_id = new
+	emit_signal("updated_id",has_id)
