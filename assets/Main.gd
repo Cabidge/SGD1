e3f1c4ac23c4f5d9a63521d1ea3270d6f5b6b6b0
wdong20 <wdong20@stuy.edu>
2020-11-25 10:29:13 -0500

    CRT Mission Failed screen

--- assets/Main.gd
+++ assets/Main.gd
@@ -9,13 +9,18 @@ onready var ui = $CanvasLayer/UI
 
 onready var door_transition = $CanvasLayer/DoorTransition
 
+onready var crt = $CanvasLayer/CRT
+onready var crt_anim = crt.get_node("AnimationPlayer")
+
 func _ready():
 	load_scene(initial_scene)
+	
+	var err = Player.connect("updated_health",self,"_on_Player_updated_health")
 
 
-func _input(event):
-	if event.is_action_pressed("reload"):
-		restart_level()
+#func _input(event):
+#	if event.is_action_pressed("reload"):
+#		change_scene(current_scene)
 
 func load_scene(scene : PackedScene):
 	if current_level != null:
@@ -33,6 +38,10 @@ func load_scene(scene : PackedScene):
 	Player.mana = Player.MAX_MANA
 	Player.has_id = false
 	ui.reset_all()
+	
+	# reset crt
+	crt_anim.play_backwards("MissionFailed")
+	crt_anim.seek(0)
 
 func change_scene(scene : PackedScene):
 	door_transition.close()
@@ -42,7 +51,13 @@ func change_scene(scene : PackedScene):
 	door_transition.open()
 
 func restart_level():
-	change_scene(current_scene)
+	crt.material.set_shader_param("aberration_amount",3)
+	yield(get_tree().create_timer(0.1),"timeout")
+	door_transition.progress = 1
+	yield(get_tree().create_timer(0.1),"timeout")
+	load_scene(current_scene)
+	yield(get_tree().create_timer(0.6),"timeout")
+	door_transition.open()
 
 
 func _on_current_level_complete(scene : PackedScene):
@@ -50,3 +65,12 @@ func _on_current_level_complete(scene : PackedScene):
 		change_scene(scene)
 	else:
 		change_scene(initial_scene)
+
+
+func _on_Player_updated_health(health):
+	if health <= 0:
+		crt_anim.play("MissionFailed")
+
+
+func _on_Retry_pressed():
+	restart_level()
