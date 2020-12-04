36baa8a7ed91e275dfda3764cd638df4e3040435
wdong20 <wdong20@stuy.edu>
2020-12-03 20:10:26 -0500

    Updated Main Menu

--- assets/ui/menu/Menu.gd
+++ assets/ui/menu/Menu.gd
@@ -1,26 +1,5 @@
-extends Sprite
-
-func _on_Timer_timeout():
-	if randf() < 0.2:
-		blink()
-	else:
-		twinkle()
-
-func blink():
-	frame = 1
-	yield(get_tree().create_timer(0.13), "timeout")
-	frame = 2
-	yield(get_tree().create_timer(0.15), "timeout")
-	frame = 1
-	yield(get_tree().create_timer(0.15), "timeout")
-	frame = 0
-
-func twinkle():
-	frame = 3 + randi() % 7
-	yield(get_tree().create_timer(0.4), "timeout")
-	frame = 0
-
+extends AnimatedSprite
 
 func stop():
 	visible = false
-	$Timer.stop()
+	playing = false
